51c01e05a43172f8cde388c48f5d8b84f0c44902
dlir2404 <127393547+dlir2404@users.noreply.github.com>
2023-12-18 09:56:11 +0700

    fix api recruitment post

--- src/controllers/recruitmentPostController.js
+++ src/controllers/recruitmentPostController.js
@@ -1,6 +1,7 @@
 const db = require('../models')
 // create main Model
 const RecruitmentPost = db.recruitmentPost
+const Skill = db.skill
 
 const addRecruitmentPost = async (req, res) => {
     try {
@@ -16,6 +17,10 @@ const addRecruitmentPost = async (req, res) => {
             dateClose: req.body.dateClose,
         };
         const recruitmentPost = await RecruitmentPost.create(info);
+        await req.body.skill?.map(async (item) => {
+            await recruitmentPost.addSkill(item)
+        })
+
         return res.status(201).json(recruitmentPost);
     } catch (error) {
         console.error(error);
@@ -41,7 +46,14 @@ const updateRecruitmentPost = async (req, res) => {
 
 const getAllRecruitmentPost = async (req, res) => {
     try {
-        let recruitmentPosts = await RecruitmentPost.findAll({ order: [['createdAt', 'DESC']] })
+        let recruitmentPosts = await RecruitmentPost.findAll({
+            order: [['createdAt', 'DESC']],
+            include: [{
+                model: Skill,
+                through: { attributes: [] },
+                attributes: ['id', 'name'],
+            }]
+        })
         res.status(200).json(recruitmentPosts)
     }
     catch (error) {
