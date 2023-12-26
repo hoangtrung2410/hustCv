a63db0b9325b03a13742707f18da94ebe8e5f2dc
Vu <longvunguyen2003@gmail.com>
2023-12-26 12:09:56 +0700

    CHANGE POST TO PUT ADD PHONENUMBER

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,9 +1,8 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'longvu',
-    PASSWORD: 'longvunduc2911',
-    DB: 'hustCV',
-    dialect: 'mysql',
+    HOST: 'mysqldb.cing4ikwlyqw.ap-southeast-1.rds.amazonaws.com',
+    USER: 'admin',
+    PASSWORD: '12345678',
+    DB: 'hustCv',
     port: 3306,
     pool: {
         max: 5,
--- src/routers/recruiterApplicationRouter.js
+++ src/routers/recruiterApplicationRouter.js
@@ -6,7 +6,7 @@ route.get('/getPosts', authMiddleware.isEmployer,controller.getRecruiterPosts);
 route.get('/getAcceptedApplications/:postId', authMiddleware.isEmployer,controller.getAcceptedApplications)
 route.get('/getPendingApplications/:postId', authMiddleware.isEmployer,controller.getPendingApplications)
 route.get('/getDetailedApplication/:applicationId', authMiddleware.isEmployer,controller.getDetailedApplication)
-route.post('/postAcceptApplication/:applicationId', authMiddleware.isEmployer,controller.postAcceptApplication)
-route.post('/postDeclineApplication/:applicationId', authMiddleware.isEmployer,controller.postDeclineApplication)
+route.put('/putAcceptApplication/:applicationId', authMiddleware.isEmployer,controller.putAcceptApplication)
+route.put('/putDeclineApplication/:applicationId', authMiddleware.isEmployer,controller.putDeclineApplication)
 route.get('/getAcceptedDetailedApplication/:applicationId', authMiddleware.isEmployer, controller.getAcceptedDetailedApplication)
 module.exports =  route;
