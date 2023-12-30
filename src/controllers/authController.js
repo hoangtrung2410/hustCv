576f600686fef84fd885eb75bb04efafa88f9091
Hoàng Trung <trung@MacBook-Pros-Trung.local>
2023-12-30 23:20:53 +0700

    update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,26 +1,24 @@
 const db = require('../models');
 const User = db.user;
 const {Op} = require("sequelize");
-const Yup = require('yup');
 const crypto = require('crypto');
+const Yup = require('yup');
 const JwtService = require("../services/jwtServices.js");
 const {BadRequestError, UnauthorizedError, ValidationError} = require("../utils/apiError.js");
 const sendMail = require('../middlerwares/sendMail.js')
-let resetUserId;
-
 const login = async (req, res) => {
     try {
         console.log(">>>>>>1")
         const schema = Yup.object().shape({
             email: Yup.string().email().required().test(
-                'is-gmail',
-                'Email must be a Gmail address',
-                (value) => {
-                    if (value) {
-                        return value.endsWith('@gmail.com');
-                    }
-                    return false;
-                }
+              'is-gmail',
+              'Email must be a Gmail address',
+              (value) => {
+                  if (value) {
+                      return value.endsWith('@gmail.com');
+                  }
+                  return false;
+              }
             ),
             password: Yup.string().required(),
         });
@@ -31,10 +29,10 @@ const login = async (req, res) => {
         } catch (e) {
             console.error(e)
             return res.status(400).json({
-                    statusCode: 400,
-                    message: "Bad Request",
-                    error: e.errors
-                }
+                  statusCode: 400,
+                  message: "Bad Request",
+                  error: e.errors
+              }
             );
         }
         let {email, password} = req.body;
@@ -54,7 +52,7 @@ const login = async (req, res) => {
                 error: 'Invalid password.'
             });
         }
-        const accessToken = JwtService.jwtSign({ userId: user.id, roleId: user.role_id }, { expiresIn: "30m" });
+        const accessToken = JwtService.jwtSign({userId: user.id, roleId: user.role_id}, {expiresIn: "-10s"});
         console.log("Generated Access Token:", accessToken);
         const millisecondsInOneDay = 24 * 60 * 60 * 1000;
         const checkDate = Date.now() - user.timeCreateRefreshToken;
@@ -62,8 +60,8 @@ const login = async (req, res) => {
         if (user.timeCreateRefreshToken === 0 || checkDate > 6 * millisecondsInOneDay) {
             refreshToken = JwtService.jwtSign({userId: user.id, roleId: user.role_id}, {expiresIn: "7d"});
             await User.update(
-                {refreshToken, timeCreateRefreshToken: Date.now()},
-                {where: {id: user.id}}
+              {refreshToken, timeCreateRefreshToken: Date.now()},
+              {where: {id: user.id}}
             );
         } else {
             console.log("Duration is not greater than 6 days");
@@ -102,12 +100,15 @@ const refreshToken = async (req, res) => {
             });
         // Check token có hợp lệ hay không
         const rs = await JwtService.jwtVerify(refreshToken)
-        console.log("rs:", rs)
+        console.log("rs:", rs._id)
         const response = await User.findOne({id: rs._id, refreshToken: refreshToken})
         console.log("response:", response)
         return res.status(200).json({
             success: response ? true : false,
-            accessToken: response ? JwtService.jwtSign(rs, {expiresIn: '1d'}) : null
+            accessToken: response ? JwtService.jwtSign({
+                userId: response.id,
+                roleId: response.role_id
+            }, {expiresIn: '1d'}) : null
         })
     } catch (e) {
         return res.status(500).json({
@@ -138,14 +139,14 @@ const forgotPassword = async (req, res) => {
     try {
         const schema = Yup.object().shape({
             email: Yup.string().email().required().test(
-                'is-gmail',
-                'Email must be a Gmail address',
-                (value) => {
-                    if (value) {
-                        return value.endsWith('@gmail.com');
-                    }
-                    return false;
-                }
+              'is-gmail',
+              'Email must be a Gmail address',
+              (value) => {
+                  if (value) {
+                      return value.endsWith('@gmail.com');
+                  }
+                  return false;
+              }
             ),
         });
         try {
@@ -153,25 +154,29 @@ const forgotPassword = async (req, res) => {
         } catch (e) {
             console.error(e)
             return res.status(400).json({
-                    statusCode: 400,
-                    message: "Bad Request",
-                    error: e.errors
-                }
+                  statusCode: 400,
+                  message: "Bad Request",
+                  error: e.errors
+              }
             );
         }
         let {email} = req.body;
-        console.log("checkemail>>>" + email)
+
         const user = await User.findOne({
             where: {
                 [Op.or]: [
-                    {email: email},
+                    {
+                        email: email,
+                        status: true
+                    },
                 ],
             },
         });
         if (!user) {
-            return res.status(400).json({
-                statusCode: 400,
-                message: "Bad Request",
+            return res.status(401).json({
+                statusCode: 401,
+                message: "Unauthorized",
+                error: 'User does not exist'
             });
         }
         console.log("user có tồn tai");
@@ -184,7 +189,7 @@ const forgotPassword = async (req, res) => {
         console.log("user da luu ");
         const passwordCode = crypto.createHash('sha256').update(verificationCode.toString()).digest('hex');
         console.log("passwordCode:", passwordCode);
-        const html = `Chúc mừng bạn đến với HustCv, đây là mã code của bạn: ${verificationCode}. Mã này sẽ hết hạn trong 15 phút.`;
+        const html = `Chúc mừng bạn đến với GoodBody, đây là mã code của bạn: ${verificationCode}. Mã này sẽ hết hạn trong 15 phút.`;
         const text = {
             email,
             html
@@ -196,7 +201,6 @@ const forgotPassword = async (req, res) => {
             message: "OK",
             data: rs
         })
-
     } catch (e) {
         return res.status(500).json({
             statusCode: 500,
@@ -205,13 +209,12 @@ const forgotPassword = async (req, res) => {
         })
     }
 }
-
 const checkCode = async (req, res) => {
     try {
         const schema = Yup.object().shape({
+            email: Yup.string().required(),
             verificationCode: Yup.number().required(),
         });
-
         try {
             await schema.validate(req.body);
         } catch (e) {
@@ -223,16 +226,16 @@ const checkCode = async (req, res) => {
             });
         }
         console.log("verificationCode");
-        const inputVerificationCode = req.body.verificationCode;
-        console.log("code:", inputVerificationCode);
+        const {email, verificationCode} = req.body;
+        console.log("code:", verificationCode);
         const hashedVerificationCode = crypto
-            .createHash("sha256")
-            .update(inputVerificationCode.toString())
-            .digest("hex");
+          .createHash("sha256")
+          .update(verificationCode.toString())
+          .digest("hex");
         console.log("hashedToken:", hashedVerificationCode);
-
         const user = await User.findOne({
             where: {
+                email: email,
                 passwordCode: hashedVerificationCode,
                 codeResetExpires: {
                     [Op.gt]: Date.now(),
@@ -240,22 +243,18 @@ const checkCode = async (req, res) => {
             },
         });
 
-        if (!user) {
+        if (!user)
             return res.status(400).json({
                 statusCode: 400,
                 message: "Bad Request",
             });
-        }
-        resetUserId = user.id
-
-        console.log("user:", user.id);
-        console.log("resetUserId:", resetUserId)
 
         res.status(200).json({
             statusCode: 200,
             message: "OK",
             notification: "Valid code",
         });
+
     } catch (e) {
         console.error(e);
         return res.status(500).json({
@@ -265,31 +264,28 @@ const checkCode = async (req, res) => {
         });
     }
 };
-
 const resetPassword = async (req, res) => {
     try {
         const schema = Yup.object().shape({
+            email: Yup.string().required(),
             newPassword: Yup.string().required().min(8),
         });
-        let newPassword = req.body.newPassword
+        let {email, newPassword} = req.body
         try {
             await schema.validate(req.body);
         } catch (e) {
             console.error(e)
             return res.status(400).json({
-                    statusCode: 400,
-                    message: "Bad Request",
-                    error: e.errors
-                }
+                  statusCode: 400,
+                  message: "Bad Request",
+                  error: e.errors
+              }
             );
         }
-
         const user = await User.findOne({
             where: {
-                [Op.or]: [
-                    {id: resetUserId},
-                ],
-            },
+                email: email
+            }
         });
         if (!user) {
             return res.status(400).json({
@@ -319,8 +315,8 @@ const resetPassword = async (req, res) => {
 module.exports = {
     login,
     logout,
-    refreshToken,
     checkCode,
     forgotPassword,
+    refreshToken,
     resetPassword
 }
