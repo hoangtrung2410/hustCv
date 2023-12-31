b3d071a8ed30f0d27c541b76e0aa7332438b011d
Hoàng Trung <trung@MacBook-Pros-Trung.local>
2023-12-31 22:34:57 +0700

    update

--- src/middlerwares/authMiddleware.js
+++ src/middlerwares/authMiddleware.js
@@ -28,7 +28,7 @@ const isEmployer = async (req, res, next) => {
             return res.status(403).json("You need sign in")
         }
         if (decoded.roleId !== 1) {
-            return res.status(401).json("You don't have employer")
+            return res.status(403).json("You don't have employer")
         }
         req.userId = decoded.userId;
         return next()
@@ -49,7 +49,7 @@ const isJobSeeker = async (req, res, next) => {
             return res.status(403).json("You need sign in")
         }
         if (decoded.roleId !== 2) {
-            return res.status(401).json("You don't have job seekers")
+            return res.status(403).json("You don't have job seekers")
         }
         req.userId = decoded.userId;
         return next()
