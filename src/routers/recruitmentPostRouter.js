a373ea2c54bdcee8b2873f9ba0bbfac28e0d3452
dlir2404 <127393547+dlir2404@users.noreply.github.com>
2024-01-03 02:49:08 +0700

    complete recruitment post

--- src/routers/recruitmentPostRouter.js
+++ src/routers/recruitmentPostRouter.js
@@ -1,11 +1,13 @@
 const recruitmentPostController = require("../controllers/recruitmentPostController")
+const authMiddleware = require("../middlerwares/authMiddleware.js")
+
 
 const router = require('express').Router()
 
-router.get('/',recruitmentPostController.getAllRecruitmentPost)
-router.get('/:id',recruitmentPostController.getOneRecruitmentPost)
-router.post('/',recruitmentPostController.addRecruitmentPost)
-router.put('/:id',recruitmentPostController.updateRecruitmentPost)
-router.delete('/:id',recruitmentPostController.deleteRecruitmentPost)
+router.get('/', authMiddleware.authMiddleware, recruitmentPostController.getAllRecruitmentPost)
+router.get('/:id', authMiddleware.authMiddleware, recruitmentPostController.getOneRecruitmentPost)
+router.post('/', authMiddleware.authMiddleware, recruitmentPostController.addRecruitmentPost)
+router.put('/:id', authMiddleware.authMiddleware, recruitmentPostController.updateRecruitmentPost)
+router.delete('/:id', authMiddleware.authMiddleware, recruitmentPostController.deleteRecruitmentPost)
 
 module.exports = router
