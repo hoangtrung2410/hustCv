c3694521251ec266952e6c5c4dfef1b05d0ae9ef
IMnhvjp <levanminh19102003@gmail.com>
2024-01-12 18:15:17 +0700

    fix

--- src/controllers/uploadApplicationController.js
+++ src/controllers/uploadApplicationController.js
@@ -1,6 +1,7 @@
 const { RDS } = require('aws-sdk');
 const db = require('../models')
 const s3 = require('../models/CV')
+const fs = require('fs');
 const { S3Client, DeleteObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3")
 
 const application = db.application;
@@ -17,74 +18,98 @@ const bucketName = 'hustcv-1';
 
 const uploadApplication = async (req, res) => {
     const userId = req.userId;
-    const postId = req.body.postId;
-    const key = Date.now() + '-' + Math.round(Math.random() * 1E9) + '-' + req.file?.originalname;
-    const fileContent = req.file.buffer;
+    const postId = req.body.recruitmentPost_id;
+    const content = req.body.content;
+    const key = req.body.CV?.split('/')[2];
+    const filePath = req.filePath;
 
     const infor = {
         CV: key,
         status: 'Đang chờ',
         user_id: userId,
-        recruitmentPost_id: postId
+        recruitmentPost_id: postId,
+        content: content
     }
-    console.log(infor);
-    const params = {
-        Bucket: bucketName,
-        Key: key,
-        Body: fileContent
-    };
 
-    s3.upload(params, async (err, data) => {
+    fs.readFile(filePath, (err, data) => {
         if (err) {
-            console.error("Error uploading file:", err);
-            res.status(404).json({ error: "loi upload" })
+          console.error('Error reading file:', err);
+          return;
         }
-        else {
-            try{
-                const old_application = await application.findOne({where: {user_id: userId, recruitmentPost_id: postId}});
-                if (old_application) {
-                    await application.update({CV: key}, {where: {user_id: userId}});
-            }
-                else {
-                    await application.create(infor);
-                }
-                res.status(200).json('upload thanh cong')
-            }
-            catch(err) {
-                res.status(404).json('loi tao moi db')
-            }
-        }
-    });
-}
-const useAvailableCv = async (req, res) => {
-    try{
-        const user_id = req.userId;
-        const postId = req.body.postId;
-        const userFile = await personalFile.findOne({where: {id: user_id}});
-        const old_application = application.findOne({where: {user_id: user_id, recruitmentPost_id: postId}});
-        const available_cv = userFile.cv;
-        if (available_cv != '') {
-            if (old_application){
-                await application.update({CV: available_cv}, {where: {user_id: user_id, recruitmentPost_id: postId}});
+        const params = {
+            Bucket: bucketName,
+            Key: key,
+            Body: data
+        };
+        s3.upload(params, async (err, data) => {
+            if (err) {
+                console.error("Error uploading file:", err);
+                res.status(404).json({ error: "loi upload" })
             }
             else {
-                const infor = {
-                    CV: available_cv,
-                    status: 'Đang chờ',
-                    user_id: user_id,
-                    recruitmentPost_id: postId
+                try{
+                    const old_application = await application.findOne({where: {user_id: userId, recruitmentPost_id: postId}});
+                    if (old_application) {
+                        await application.update({CV: key}, {where: {user_id: userId}});
                 }
-                await application.create(infor);
+                    else {
+                        await application.create(infor);
+                    }
+                    res.status(200).json('upload thanh cong')
+                }
+                catch(err) {
+                    res.status(404).json('loi tao moi db')
+                }
             }
-            res.status(200).json('upload thanh cong');
+        });
+    })
+}
+const useAvailableCv = async (req, res) => {
+    // try{
+    //     const user_id = req.userId;
+    //     const postId = req.body.postId;
+    //     console.log(postId)
+    //     const userFile = await personalFile.findOne({where: {id: user_id}});
+    //     const old_application = application.findOne({where: {user_id: user_id, recruitmentPost_id: postId}});
+    //     const available_cv = userFile.cv;
+    //     if (available_cv != '') {
+    //         if (old_application){
+    //             await application.update({CV: available_cv}, {where: {user_id: user_id, recruitmentPost_id: postId}});
+    //         }
+    //         else {
+    //             const infor = {
+    //                 CV: available_cv,
+    //                 status: 'Đang chờ',
+    //                 user_id: user_id,
+    //                 recruitmentPost_id: postId
+    //             }
+    //             await application.create(infor);
+    //         }
+    //         res.status(200).json();
+    //     }
+    //     else {
+    //         res.status(404).json('not available CV');
+    //     }
+    // }
+    // catch (err) {
+    //     res.status(404).json('loi server');
+    // }
+        try{
+            const user_id = req.userId;
+            const postId = req.body.recruitmentPost_id;
+            const userFile = await personalFile.findOne({where: {id: user_id}});
+            const available_cv = userFile.cv;
+            if (available_cv != '') {
+                const fakePath = 'src/public/' + available_cv
+                res.status(200).json({ pdfPath: fakePath })
+            }
+            else {
+                res.status(404).json('loi')
+            }
         }
-        else {
-            res.status(404).json('not available CV');
+        catch(err) {
+            res.status(404).json('loi server');
         }
-    }
-    catch (err) {
-        res.status(404).json('loi server');
-    }
 }
 
 module.exports = {
--- src/routers/uploadApplicationRouter.js
+++ src/routers/uploadApplicationRouter.js
@@ -1,10 +1,9 @@
 const uploadApplicationController = require("../controllers/uploadApplicationController")
 const authMidleware = require("../middlerwares/authMiddleware")
 const multer = require('multer');
-const upload = multer();
 const router = require('express').Router()
 
-router.post('/upload', authMidleware.isJobSeeker, upload.single('file'), uploadApplicationController.uploadApplication);
+router.post('/submit', authMidleware.isJobSeeker, uploadApplicationController.uploadApplication);
 router.post('/available', authMidleware.isJobSeeker, uploadApplicationController.useAvailableCv);
 
 module.exports = router
